22c7a93314c8f25dda6a1b20742189b8ca972a86
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2018-06-11 15:30:32 +0300

    sample from https://github.com/netgloo/spring-boot-samples/tree/master/spring-boot-mysql-springdatajpa-hibernate

---
+++ chapter-08/08-7_spring-boot-mysql-springdatajpa-hibernate/src/main/java/netgloo/controllers/UserController.java
@@ -0,0 +1,70 @@
+package netgloo.controllers;
+
+import netgloo.models.User;
+import netgloo.models.UserDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class UserController {
+
+  @Autowired
+  private UserDao userDao;
+
+
+  @RequestMapping("/create")
+  @ResponseBody
+  public String create(String email, String name) {
+    User user = null;
+    try {
+      user = new User(email, name);
+      userDao.save(user);
+    } catch (Exception ex) {
+      return "Error creating the user: " + ex.toString();
+    }
+    return "User succesfully created! (id = " + user.getId() + ")";
+  }
+
+  @RequestMapping("/delete")
+  @ResponseBody
+  public String delete(long id) {
+    try {
+      User user = new User(id);
+      userDao.delete(user);
+    } catch (Exception ex) {
+      return "Error deleting the user: " + ex.toString();
+    }
+    return "User successfully deleted!";
+  }
+
+  @RequestMapping("/update")
+  @ResponseBody
+  public String updateUser(long id, String email, String name) {
+    try {
+      User user = userDao.findOne(id);
+      user.setEmail(email);
+      user.setName(name);
+      userDao.save(user);
+    } catch (Exception ex) {
+      return "Error updating the user: " + ex.toString();
+    }
+    return "User successfully updated!";
+  }
+
+  @RequestMapping("/get-by-email")
+  @ResponseBody
+  public String getByEmail(String email) {
+    String userId;
+    try {
+      User user = userDao.findByEmail(email);
+      userId = String.valueOf(user.getId());
+    } catch (Exception ex) {
+      return "User not found";
+    }
+    return "The user id is: " + userId;
+  }
+}
---
+++ chapter-08/08-7_spring-boot-mysql-springdatajpa-hibernate/src/main/java/netgloo/models/UserDao.java
@@ -0,0 +1,10 @@
+package netgloo.models;
+
+import javax.transaction.Transactional;
+import org.springframework.data.repository.CrudRepository;
+
+
+@Transactional
+public interface UserDao extends CrudRepository<User, Long> {
+  public User findByEmail(String email);
+}
