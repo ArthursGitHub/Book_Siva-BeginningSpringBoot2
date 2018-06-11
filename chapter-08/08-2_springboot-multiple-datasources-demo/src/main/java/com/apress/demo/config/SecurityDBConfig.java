7d4f6e21e4fcbe840a21f4f1e37bcf4183d4847e
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2018-06-11 16:32:53 +0300

    small fix

--- chapter-08/08-2_springboot-multiple-datasources-demo/src/main/java/com/apress/demo/config/SecurityDBConfig.java
+++ chapter-08/08-2_springboot-multiple-datasources-demo/src/main/java/com/apress/demo/config/SecurityDBConfig.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
