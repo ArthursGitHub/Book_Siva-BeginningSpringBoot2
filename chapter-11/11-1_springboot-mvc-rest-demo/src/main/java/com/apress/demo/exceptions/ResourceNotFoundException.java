a1cb68f66f00ed7d2b122cc7fc21b15f3bdd0dc7
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2018-06-12 22:50:04 +0300

    chapter 11

---
+++ chapter-11/11-1_springboot-mvc-rest-demo/src/main/java/com/apress/demo/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,21 @@
+package com.apress.demo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  public ResourceNotFoundException() {
+    this("Resource not found!");
+  }
+
+  public ResourceNotFoundException(String message) {
+    this(message, null);
+  }
+
+  public ResourceNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
