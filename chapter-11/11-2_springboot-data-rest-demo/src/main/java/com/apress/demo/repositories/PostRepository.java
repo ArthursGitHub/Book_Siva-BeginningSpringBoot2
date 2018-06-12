bd30987425bbab1d6ab75d139fe53a83e16df918
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2018-06-12 23:45:47 +0300

    chapter 11

---
+++ chapter-11/11-2_springboot-data-rest-demo/src/main/java/com/apress/demo/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.apress.demo.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apress.demo.entities.Post;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
+  List<Post> findByTitleLike(String query);
+}
