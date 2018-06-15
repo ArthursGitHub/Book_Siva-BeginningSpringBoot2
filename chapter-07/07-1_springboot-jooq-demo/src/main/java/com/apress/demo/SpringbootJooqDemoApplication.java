8408975038ccc2c154afb6b789dd0587c31f993c
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2018-06-15 19:28:50 +0300

    chapter 7

--- chapter-07/07-1_springboot-jooq-demo/src/main/java/com/apress/demo/SpringbootJooqDemoApplication.java
+++ chapter-07/07-1_springboot-jooq-demo/src/main/java/com/apress/demo/SpringbootJooqDemoApplication.java
@@ -3,17 +3,9 @@ package com.apress.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author Siva
- *
- */
 @SpringBootApplication
-public class SpringbootJooqDemoApplication
-{
-
-	public static void main(String[] args)
-	{
-		SpringApplication.run(SpringbootJooqDemoApplication.class, args);
-	}
-
+public class SpringbootJooqDemoApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootJooqDemoApplication.class, args);
+  }
 }
--- chapter-07/07-1_springboot-jooq-demo/src/test/java/com/apress/demo/SpringbootJooqDemoApplicationTests.java
+++ chapter-07/07-1_springboot-jooq-demo/src/test/java/com/apress/demo/SpringbootJooqDemoApplicationTests.java
@@ -17,69 +17,61 @@ import com.apress.demo.entities.Comment;
 import com.apress.demo.entities.Post;
 import com.apress.demo.services.PostService;
 
-/**
- * @author Siva
- *
- */
 @RunWith(SpringRunner.class)
 @SpringBootTest//(classes = SpringbootJooqDemoApplication.class)
-public class SpringbootJooqDemoApplicationTests
-{
+public class SpringbootJooqDemoApplicationTests {
 
-	@Autowired
-    private PostService postService;
-     
-    @Test
-    public void findAllPosts()  {
-        List<Post> posts = postService.getAllPosts();
-        assertNotNull(posts);
-        assertTrue(!posts.isEmpty());
-        for (Post post : posts)
-        {
-            System.err.println(post);
-        }
+  @Autowired
+  private PostService postService;
+
+  @Test
+  public void findAllPosts() {
+    List<Post> posts = postService.getAllPosts();
+    assertNotNull(posts);
+    assertTrue(!posts.isEmpty());
+    for (Post post : posts) {
+      System.err.println(post);
     }
-     
-    @Test
-    public void findPostById()  {
-        Post post = postService.getPost(1);
-        assertNotNull(post);
-        System.out.println(post);
-        List<Comment> comments = post.getComments();
-        System.out.println(comments);
-         
-    }
-     
-    @Test
-    public void createPost() {
-        Post post = new Post(0, "My new Post", 
-                            "This is my new test post", 
-                            new Timestamp(System.currentTimeMillis()));
-        Post savedPost = postService.createPost(post);
-        Post newPost = postService.getPost(savedPost.getId());
-        assertEquals("My new Post", newPost.getTitle());
-        assertEquals("This is my new test post", newPost.getContent());
-    }
-     
-    @Test
-    public void createComment() {
-        Integer postId = 1;
-        Comment comment = new Comment(0, postId, "User4", 
-                                "user4@gmail.com", "This is my new comment on post1", 
-                                new Timestamp(System.currentTimeMillis()));
-        Comment savedComment = postService.createComment(comment);
-        Post post = postService.getPost(postId);
-        List<Comment> comments = post.getComments();
-        assertNotNull(comments);
-        for (Comment comm : comments)
-        {
-            if(savedComment.getId() == comm.getId()){
-                assertEquals("User4", comm.getName());
-                assertEquals("user4@gmail.com", comm.getEmail());
-                assertEquals("This is my new comment on post1", comm.getContent());
-            }
-        }
-         
+  }
+
+  @Test
+  public void findPostById() {
+    Post post = postService.getPost(1);
+    assertNotNull(post);
+    System.out.println(post);
+    List<Comment> comments = post.getComments();
+    System.out.println(comments);
+
+  }
+
+  @Test
+  public void createPost() {
+    Post post = new Post(0, "My new Post",
+            "This is my new test post",
+            new Timestamp(System.currentTimeMillis()));
+    Post savedPost = postService.createPost(post);
+    Post newPost = postService.getPost(savedPost.getId());
+    assertEquals("My new Post", newPost.getTitle());
+    assertEquals("This is my new test post", newPost.getContent());
+  }
+
+  @Test
+  public void createComment() {
+    Integer postId = 1;
+    Comment comment = new Comment(0, postId, "User4",
+            "user4@gmail.com", "This is my new comment on post1",
+            new Timestamp(System.currentTimeMillis()));
+    Comment savedComment = postService.createComment(comment);
+    Post post = postService.getPost(postId);
+    List<Comment> comments = post.getComments();
+    assertNotNull(comments);
+    for (Comment comm : comments) {
+      if (savedComment.getId() == comm.getId()) {
+        assertEquals("User4", comm.getName());
+        assertEquals("user4@gmail.com", comm.getEmail());
+        assertEquals("This is my new comment on post1", comm.getContent());
+      }
     }
 
+  }
 }
